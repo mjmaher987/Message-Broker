0c7291e5e99002cf3035c696abd43f0c70877978
mjmaher987 <mjmaher987@gmail.com>
2024-01-12 15:44:30 +0330

    implement the overall structure of Producer and Consumer

--- message-broker-client/src/main/java/sad/project/Consumer.java
+++ message-broker-client/src/main/java/sad/project/Consumer.java
@@ -1,5 +1,27 @@
 package sad.project;
 
-public class Consumer {
+import java.util.concurrent.BlockingQueue;
 
+public class Consumer implements Runnable {
+    private final BlockingQueue<Message> queue;
+
+    public Consumer(BlockingQueue<Message> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Message message = queue.take();
+                processMessage(message);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void processMessage(Message message) {
+        System.out.println("Received message: " + message.toString());
+    }
 }
--- message-broker-client/src/main/java/sad/project/Producer.java
+++ message-broker-client/src/main/java/sad/project/Producer.java
@@ -1,5 +1,32 @@
 package sad.project;
 
-public class Producer {
+import java.time.LocalDateTime;
+import java.util.concurrent.BlockingQueue;
 
+public class Producer implements Runnable {
+    private final BlockingQueue<Message> queue;
+
+    public Producer(BlockingQueue<Message> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Message message = createMessage();
+                queue.put(message);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private Message createMessage() {
+//        It depends on the server
+        String data = "Hello from the Producer!";
+        LocalDateTime timeArrived = LocalDateTime.now();
+        return new Message(data, timeArrived);
+    }
 }
