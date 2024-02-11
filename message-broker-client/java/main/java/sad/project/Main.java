97a3e013fbfde6a149cc82970136a662484e55df
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2024-02-12 00:37:58 +0330

    fix java client

--- message-broker-client/java/main/java/sad/project/Main.java
+++ message-broker-client/java/main/java/sad/project/Main.java
@@ -1,7 +1,24 @@
 package sad.project;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
+        Producer producer = new Producer();
 
+        Consumer consumer = new Consumer();
+
+        producer.push(Message.builder().key("myKey").value("myValue").build());
+        producer.push(Message.builder().key("myKey").value("myValue2").build());
+        producer.push(Message.builder().key("myKey").value("myValue3").build());
+
+//        final Message pull = consumer.pull();
+
+        Function<Message, Void> printInput = (Message text) -> {
+            System.out.println(text);
+            return null;
+        };
+
+        consumer.subscribe(printInput, 1000);
     }
 }
