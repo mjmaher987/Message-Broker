edbdc24fba94862daaf3cc9b5161e9b1efd55006
ckoorosh <mofayezi.m@gmail.com>
2024-02-11 17:54:41 +0000

    Adding test 3

---
+++ message-broker-server/tests/3/test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Test for fault tolerance: Second node goes down, third one goes up and then goes down.
+
+BASE_DIR=/home/marmof/SAD/Message-Broker/message-broker-server
+
+python $BASE_DIR/coordinator/manage.py flush --noinput
+python $BASE_DIR/coordinator/manage.py migrate
+python $BASE_DIR/coordinator/manage.py runserver 0.0.0.0:7000 &
+sleep 5
+echo 'Coordinator Connected!'
+
+SETTING_PORT=8000
+export SETTING_PORT=$SETTING_PORT
+mkdir $BASE_DIR/server/$SETTING_PORT
+cp $BASE_DIR/server/server/settings.py $BASE_DIR/server/$SETTING_PORT/settings.py
+python $BASE_DIR/server/manage.py migrate --settings=$SETTING_PORT.settings
+python $BASE_DIR/server/manage.py runserver 0.0.0.0:$SETTING_PORT --settings=$SETTING_PORT.settings &
+sleep 5
+python $BASE_DIR/server/connect.py &
+sleep 2
+echo $SETTING_PORT 'Connected!'
+
+SETTING_PORT=8001
+export SETTING_PORT=$SETTING_PORT
+mkdir $BASE_DIR/server/$SETTING_PORT
+cp $BASE_DIR/server/server/settings.py $BASE_DIR/server/$SETTING_PORT/settings.py
+python $BASE_DIR/server/manage.py migrate --settings=$SETTING_PORT.settings
+python $BASE_DIR/server/manage.py runserver 0.0.0.0:$SETTING_PORT --settings=$SETTING_PORT.settings &
+sleep 5
+python $BASE_DIR/server/connect.py &
+sleep 2
+echo $SETTING_PORT 'Connected!'
+
+kill $!
+echo $SETTING_PORT 'Disconnected!'
+
+SETTING_PORT=8002
+export SETTING_PORT=$SETTING_PORT
+mkdir $BASE_DIR/server/$SETTING_PORT
+cp $BASE_DIR/server/server/settings.py $BASE_DIR/server/$SETTING_PORT/settings.py
+python $BASE_DIR/server/manage.py migrate --settings=$SETTING_PORT.settings
+python $BASE_DIR/server/manage.py runserver 0.0.0.0:$SETTING_PORT --settings=$SETTING_PORT.settings &
+sleep 5
+python $BASE_DIR/server/connect.py &
+sleep 2
+echo $SETTING_PORT 'Connected!'
+
+python $BASE_DIR/tests/3/test_push.py
+
+kill $!
+
+python $BASE_DIR/tests/3/test_pull.py
+
+rm -r $BASE_DIR/server/8000
+rm -r $BASE_DIR/server/8001
+rm -r $BASE_DIR/server/8002
+
+pkill -9 python
