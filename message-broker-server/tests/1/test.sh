9e8e3d99695171db2a47ec92026e94cb47d3195a
ckoorosh <mofayezi.m@gmail.com>
2024-02-11 17:39:19 +0000

    Adding tests

---
+++ message-broker-server/tests/1/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BASE_DIR=/home/marmof/SAD/Message-Broker/message-broker-server
+
+python $BASE_DIR/coordinator/manage.py flush --noinput
+python $BASE_DIR/coordinator/manage.py migrate
+python $BASE_DIR/coordinator/manage.py runserver 0.0.0.0:7000 &
+sleep 5
+echo 'Coordinator Connected!'
+
+SETTING_PORT=8000
+export SETTING_PORT=$SETTING_PORT
+mkdir $BASE_DIR/server/$SETTING_PORT
+cp $BASE_DIR/server/server/settings.py $BASE_DIR/server/$SETTING_PORT/settings.py
+python $BASE_DIR/server/manage.py migrate --settings=$SETTING_PORT.settings
+python $BASE_DIR/server/manage.py runserver 0.0.0.0:$SETTING_PORT --settings=$SETTING_PORT.settings &
+sleep 5
+python $BASE_DIR/server/connect.py &
+sleep 2
+echo '8000 Connected!'
+
+SETTING_PORT=8001
+export SETTING_PORT=$SETTING_PORT
+mkdir $BASE_DIR/server/$SETTING_PORT
+cp $BASE_DIR/server/server/settings.py $BASE_DIR/server/$SETTING_PORT/settings.py
+python $BASE_DIR/server/manage.py migrate --settings=$SETTING_PORT.settings
+python $BASE_DIR/server/manage.py runserver 0.0.0.0:$SETTING_PORT --settings=$SETTING_PORT.settings &
+sleep 5
+python $BASE_DIR/server/connect.py &
+sleep 2
+echo '8001 Connected!'
+
+python $BASE_DIR/tests/1/test.py
+
+rm -r $BASE_DIR/server/8000
+rm -r $BASE_DIR/server/8001
+
+pkill -9 python
