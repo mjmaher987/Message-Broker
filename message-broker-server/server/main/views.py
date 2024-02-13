419da308960a6a3cb9d13810536b70d6ee38ce45
mjmaher987 <mjmaher987@gmail.com>
2024-02-14 00:12:37 +0330

    prometheus added

--- message-broker-server/server/main/views.py
+++ message-broker-server/server/main/views.py
@@ -3,6 +3,14 @@ from .models import *
 import json
 from threading import Lock
 
+from prometheus_client import Counter, Gauge, Histogram
+
+# Prometheus metrics
+total_nodes = Gauge('total_nodes', 'Total number of nodes')
+message_size = Histogram('message_size_bytes', 'Size of messages in bytes')
+push_counter = Counter('push_requests_total', 'Total number of push requests')
+pull_counter = Counter('pull_requests_total', 'Total number of pull requests')
+
 
 lock = Lock()
 
@@ -16,6 +24,7 @@ def message(request):
         server, _ = Server.objects.get_or_create(port=port)
         server.is_leader = True
         server.save()
+        total_nodes.set(len(nodes)) # monitoring
         for node in nodes:
             Node.objects.get_or_create(ip=node[0], port=node[1])
         return HttpResponse(status=200)
@@ -23,6 +32,7 @@ def message(request):
         with lock:
             data = message['data']
             node, _ = Node.objects.get_or_create(ip=data['node'][0], port=data['node'][1])
+            total_nodes.inc()  # for monitoring
             if 'pair' in data:
                 pair, _ = Node.objects.get_or_create(ip=data['pair'][0], port=data['pair'][1])
                 node.pair = pair
@@ -35,6 +45,7 @@ def message(request):
             data = message['data']
             node = Node.objects.get(ip=data['node'][0], port=data['node'][1])
             node.is_alive = False
+            total_nodes.dec()  # for monitoring
             if node.pair:
                 node.pair.pair = None
                 node.pair.save()
@@ -43,12 +54,14 @@ def message(request):
     elif message['type'] == 'forward':
         message = message['data']
         Message.objects.create(key=str(message['key']), value=message['value'].encode())
+        message_size.observe(len(json.dumps(message)))  # for monitoring
         return HttpResponse(status=200)
     elif message['type'] == 'forward_replica':
         message = message['data']
         Message.objects.create(key=str(message['key']), value=message['value'].encode(), is_replica=True)
         return HttpResponse(status=200)
     elif message['type'] == 'pull':
+        pull_counter.inc()  # for monitoring
         message = Message.objects.filter(pulled=False, is_replica=False).earliest('timestamp')
         message.pulled = True
         message.save()
@@ -68,12 +81,14 @@ def push(request):
             key = str(message['key'])
             value = message['value']
             server.forward_message({'key': key, 'value': value})
+            push_counter.inc()  # for monitoring
             return HttpResponse(status=200)
     return HttpResponse(status=403)
 
 def pull(request):
     if request.method == 'POST':
         with lock:
+            pull_counter.inc()  # for monitoring
             server = Server.objects.filter(is_leader=True).first()
             message = server.get_message()
             if message:
