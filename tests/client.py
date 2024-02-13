d957bd05f7e010cdc62c8d795d4a565164b0d557
ckoorosh <mofayezi.m@gmail.com>
2024-02-13 14:21:30 +0000

    Adding overall tests

---
+++ tests/client.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from threading import Thread, Lock
+import time
+
+
+BASE_URL = 'http://127.0.0.1:8000/'
+INTERVAL = 500
+lock = Lock()
+
+
+def pull():
+    with lock:
+        response = requests.post(BASE_URL + 'pull/')
+        if response.text:
+            message = response.json()
+            return message['key'], message['value'].encode()
+        return None, None
+
+def push(key, value):
+    message = json.dumps({'key': key, 'value': value.decode("utf-8")})
+    requests.post(BASE_URL + 'push/', data=message)
+
+def subscribe(action):
+    subscriber_thread = Thread(target=subscribe_thread, args=(action, INTERVAL))
+    subscriber_thread.start()
+
+def subscribe_thread(action, interval):
+    while True:
+        key, value = pull()
+        if key:
+            action(key, value)
+        time.sleep(interval / 1000)
